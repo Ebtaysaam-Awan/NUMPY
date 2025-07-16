667450523d65f99a55ea63c3ebb59e91ad0ace42
Ebtaysaam-Awan <meerawan46@gmail.com>
2025-07-16 11:51:44 +0500

    This is update

--- 01_Solution.py
+++ 01_Solution.py
@@ -1,1 +1,15 @@
-print("Hello world")
+# creating sequece of numbers in numpy
+# arrange
+# arrange(start,stop,step )
+import numpy as np
+arr= np.arange(1,10,3)
+print(arr)
+
+# creating identity matrics
+# eye(size)
+
+identity_matrics = np.eye(3)
+print(identity_matrics)
+
+zero = np.zeros((2,3))
+print(zero)
