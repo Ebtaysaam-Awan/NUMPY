967386fa5305409489fd216281cd9f0b042839ef
Ebtaysaam-Awan <meerawan46@gmail.com>
2025-07-18 14:57:40 +0500

    updates

--- Broadcasting and vectorization/practice.py
+++ Broadcasting and vectorization/practice.py
@@ -26,3 +26,17 @@ print (result)
 arr= np.array([10,20,30])
 multiplied = arr*4
 print(multiplied)
+
+# numpy has builtin function to handle missing values
+# np.isnan -> detect missing values
+# np.isinf -> to detect infinite values
+# Syntax
+# np.isnan(array)
+
+
+arr = np.array([1,2,np.nan,4 ,np.nan])
+print(np.isnan(arr))
+
+
+# 
+print(np.nan_to_num(arr))
